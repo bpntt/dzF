23d0ba3038ed03f9b2c1100b08b203d9f4b092ab
BPNTT <haataya@163.com>
2023-02-19 23:32:17 +0600

    final

---
+++ 002/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.WriteLine("Wrtie Your number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Wrtie Your number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Check(number1, number2);
+total(number1, number2);
+
+void total(int a, int b)
+{
+    Console.Write(Sum(a - 1, b));
+}
+void Check (int a, int b)
+{
+    if( a<0) 
+    { 
+        Console.WriteLine("Number cannot be less then 0");
+    return;
+    }
+    if ( b<0)
+    {  Console.WriteLine("Number cannot be less then 0");
+    return;
+    }
+}
+
+int Sum(int a, int b)
+{
+    
+    int summ = a;
+    if (a == b)
+        return 0;
+    else
+    {
+        a++;
+        summ = a + Sum(a,b);
+        return summ;
+    }
+}
---
+++ 003/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью
+рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29
+*/
+Console.WriteLine("Wrtie Your number");
+double number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Wrtie Your number");
+double number2 = Convert.ToInt32(Console.ReadLine());
+AkFunction(number1, number2);
+Console.WriteLine($"{AkFunction(number1, number2)}");
+
+double AkFunction(double a, double b)
+{
+    if (a == 0) return b + 1;
+    if (a != 0 && b == 0) return AkFunction(a - 1, 1);
+    if (a > 0 && b > 0) return AkFunction(a - 1, AkFunction(a, b - 1));
+    return AkFunction(a, b);
+}
+
